212f8ee21f1b53c0158364777eb1f921d5be89a1
dwiseptiann9 <anandadwiseptianromadon@gmail.com>
2024-01-15 16:11:13 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,7 +70,19 @@ tabs.forEach(tab =>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio_container', {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
